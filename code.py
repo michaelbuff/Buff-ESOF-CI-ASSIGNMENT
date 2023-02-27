78917edf924d4f312cd4e9167fc89d0e22cf2270
michaelbuff <michael.t.buffington@gmail.com>
2023-02-27 10:24:30 -0700

    added test to code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 def add(a, b):
-    return a +b
+    return a + b
 
 def test_add():
     assert add(2,3) == 5
@@ -8,7 +8,7 @@ def test_add():
 
 
 def main():
-    print("Hope this is good enough")
+    test_add()
 
 if __name__ == "__main__":
     main()
